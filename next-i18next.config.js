956001a46ea71417682b383d67bb2acba0f54698
Sudhir Dadwal <sudhir.dadwal@gmail.com>
2024-02-09 10:32:16 +0530

    Deleted Old Code

--- next-i18next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    i18n: {
-        defaultLocale: 'en',
-        locales: ['en', 'es', 'fr'], // Add your desired languages here
-        localePath: 'public/static/locales',
-    },
-};
--- src/pages/_app.jsx
+++
@@ -1,8 +0,0 @@
-import '../styles/globals.css';
-import { appWithTranslation } from 'next-i18next';
-
-function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default appWithTranslation(App);
